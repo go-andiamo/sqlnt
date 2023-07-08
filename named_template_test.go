abd0874a074548b5ee661ad5bc0645c53136879e
marrow16 <martin.rowlinson16@gmail.com>
2023-07-08 12:12:41 +0100

    First release

--- named_template_test.go
+++ named_template_test.go
@@ -1,8 +1,13 @@
 package sqlnt
 
 import (
+	"context"
+	"database/sql"
+	"errors"
 	"fmt"
+	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"testing"
 	"time"
 )
@@ -16,7 +21,7 @@ func TestNamedTemplate(t *testing.T) {
 		expectArgNamesCount int
 		option              Option
 		omissibleArgs       []string
-		inArgs              map[string]any
+		inArgs              []any
 		expectArgsError     bool
 		expectOutArgs       []any
 	}{
@@ -25,10 +30,12 @@ func TestNamedTemplate(t *testing.T) {
 			expectStatement:     `INSERT INTO table (col_a, col_b, col_c) VALUES(?, ?, ?)`,
 			expectArgsCount:     3,
 			expectArgNamesCount: 3,
-			inArgs: map[string]any{
-				"a":   "a value",
-				"bb":  "bb value",
-				"ccc": "ccc value",
+			inArgs: []any{
+				map[string]any{
+					"a":   "a value",
+					"bb":  "bb value",
+					"ccc": "ccc value",
+				},
 			},
 			expectOutArgs: []any{"a value", "bb value", "ccc value"},
 		},
@@ -38,12 +45,116 @@ func TestNamedTemplate(t *testing.T) {
 			option:              MySqlOption,
 			expectArgsCount:     3,
 			expectArgNamesCount: 3,
-			inArgs: map[string]any{
-				"a":   "a value",
-				"bb":  "bb value",
-				"ccc": "ccc value",
+			inArgs: []any{
+				map[string]any{
+					"a":   "a value",
+					"bb":  "bb value",
+					"ccc": "ccc value",
+				},
 			},
 			expectOutArgs: []any{"a value", "bb value", "ccc value"},
+		},
+		{
+			statement:           `INSERT INTO table (col_a, col_b, col_c) VALUES(:a, :bb, :ccc)`,
+			expectStatement:     `INSERT INTO table (col_a, col_b, col_c) VALUES(?, ?, ?)`,
+			option:              MySqlOption,
+			expectArgsCount:     3,
+			expectArgNamesCount: 3,
+			inArgs: []any{
+				map[string]any{
+					"a": "a value",
+				},
+				sql.NamedArg{
+					Name:  "bb",
+					Value: "bb value",
+				},
+				&sql.NamedArg{
+					Name:  "ccc",
+					Value: "ccc value",
+				},
+			},
+			expectOutArgs: []any{"a value", "bb value", "ccc value"},
+		},
+		{
+			statement:           `INSERT INTO table (col_a, col_b, col_c) VALUES(:a, :bb, :ccc)`,
+			expectStatement:     `INSERT INTO table (col_a, col_b, col_c) VALUES(?, ?, ?)`,
+			option:              MySqlOption,
+			expectArgsCount:     3,
+			expectArgNamesCount: 3,
+			inArgs: []any{
+				struct {
+					A string `json:"a"`
+					B string `json:"bb"`
+					C string `json:"ccc"`
+				}{
+					"a value",
+					"bb value",
+					"ccc value",
+				},
+			},
+			expectOutArgs: []any{"a value", "bb value", "ccc value"},
+		},
+		{
+			statement:           `INSERT INTO table (col_a, col_b, col_c) VALUES(:a, :bb, :ccc)`,
+			expectStatement:     `INSERT INTO table (col_a, col_b, col_c) VALUES(?, ?, ?)`,
+			option:              MySqlOption,
+			expectArgsCount:     3,
+			expectArgNamesCount: 3,
+			inArgs: []any{
+				map[string]int{
+					"a":   3,
+					"bb":  2,
+					"ccc": 1,
+				},
+			},
+			expectOutArgs: []any{3, 2, 1},
+		},
+		{
+			statement:           `INSERT INTO table (col_a, col_b, col_c) VALUES(:a, :bb, :ccc)`,
+			expectStatement:     `INSERT INTO table (col_a, col_b, col_c) VALUES(?, ?, ?)`,
+			option:              MySqlOption,
+			expectArgsCount:     3,
+			expectArgNamesCount: 3,
+			inArgs: []any{
+				map[any]int{
+					"a":   3,
+					"bb":  2,
+					"ccc": 1,
+				},
+			},
+			expectOutArgs: []any{3, 2, 1},
+		},
+		{
+			statement:           `INSERT INTO table (col_a, col_b, col_c) VALUES(:a, :bb, :ccc)`,
+			expectStatement:     `INSERT INTO table (col_a, col_b, col_c) VALUES(?, ?, ?)`,
+			option:              MySqlOption,
+			expectArgsCount:     3,
+			expectArgNamesCount: 3,
+			inArgs: []any{
+				map[any]int{
+					"a": 3,
+					2:   2, // key is not a string!
+				},
+			},
+			expectArgsError: true,
+		},
+		{
+			statement:           `INSERT INTO table (col_a, col_b, col_c) VALUES(:a, :bb, :ccc)`,
+			expectStatement:     `INSERT INTO table (col_a, col_b, col_c) VALUES(?, ?, ?)`,
+			option:              MySqlOption,
+			expectArgsCount:     3,
+			expectArgNamesCount: 3,
+			inArgs:              []any{"not a map"},
+			expectArgsError:     true,
+		},
+		{
+			statement:           `INSERT INTO table (col_a, col_b, col_c) VALUES(:a, :bb, :ccc)`,
+			expectStatement:     `INSERT INTO table (col_a, col_b, col_c) VALUES(?, ?, ?)`,
+			option:              MySqlOption,
+			expectArgsCount:     3,
+			expectArgNamesCount: 3,
+			inArgs:              []any{&unmarshalable{}},
+			expectArgsError:     true,
 		},
 		{
 			statement:           `INSERT INTO table (col_a, col_b, col_c) VALUES(:a, :bb, :ccc)`,
@@ -51,10 +162,12 @@ func TestNamedTemplate(t *testing.T) {
 			option:              PostgresOption,
 			expectArgsCount:     3,
 			expectArgNamesCount: 3,
-			inArgs: map[string]any{
-				"a":   "a value",
-				"bb":  "bb value",
-				"ccc": "ccc value",
+			inArgs: []any{
+				map[string]any{
+					"a":   "a value",
+					"bb":  "bb value",
+					"ccc": "ccc value",
+				},
 			},
 			expectOutArgs: []any{"a value", "bb value", "ccc value"},
 		},
@@ -63,9 +176,11 @@ func TestNamedTemplate(t *testing.T) {
 			expectStatement:     `INSERT INTO table (col_a, col_b, col_c) VALUES(?, ?, ?)`,
 			expectArgsCount:     3,
 			expectArgNamesCount: 2,
-			inArgs: map[string]any{
-				"a": "a value",
-				"b": "b value",
+			inArgs: []any{
+				map[string]any{
+					"a": "a value",
+					"b": "b value",
+				},
 			},
 			expectOutArgs: []any{"a value", "b value", "a value"},
 		},
@@ -74,8 +189,10 @@ func TestNamedTemplate(t *testing.T) {
 			expectStatement:     `INSERT INTO table (col_a, col_b, col_c) VALUES(?, ?, ?)`,
 			expectArgsCount:     3,
 			expectArgNamesCount: 2,
-			inArgs: map[string]any{
-				"a": "a value",
+			inArgs: []any{
+				map[string]any{
+					"a": "a value",
+				},
 			},
 			expectArgsError: true,
 		},
@@ -85,9 +202,11 @@ func TestNamedTemplate(t *testing.T) {
 			option:              PostgresOption,
 			expectArgsCount:     2,
 			expectArgNamesCount: 2,
-			inArgs: map[string]any{
-				"a": "a value",
-				"b": "b value",
+			inArgs: []any{
+				map[string]any{
+					"a": "a value",
+					"b": "b value",
+				},
 			},
 			expectOutArgs: []any{"a value", "b value"},
 		},
@@ -97,8 +216,10 @@ func TestNamedTemplate(t *testing.T) {
 			option:              PostgresOption,
 			expectArgsCount:     2,
 			expectArgNamesCount: 2,
-			inArgs: map[string]any{
-				"a": "a value",
+			inArgs: []any{
+				map[string]any{
+					"a": "a value",
+				},
 			},
 			expectArgsError: true,
 		},
@@ -109,8 +230,10 @@ func TestNamedTemplate(t *testing.T) {
 			expectArgsCount:     2,
 			expectArgNamesCount: 2,
 			omissibleArgs:       []string{"b"},
-			inArgs: map[string]any{
-				"a": "a value",
+			inArgs: []any{
+				map[string]any{
+					"a": "a value",
+				},
 			},
 			expectArgsError: false,
 			expectOutArgs:   []any{"a value", nil},
@@ -122,7 +245,7 @@ func TestNamedTemplate(t *testing.T) {
 			expectArgsCount:     2,
 			expectArgNamesCount: 2,
 			omissibleArgs:       []string{},
-			inArgs:              map[string]any{},
+			inArgs:              []any{map[string]any{}},
 			expectArgsError:     false,
 			expectOutArgs:       []any{nil, nil},
 		},
@@ -131,8 +254,10 @@ func TestNamedTemplate(t *testing.T) {
 			expectStatement:     `UPDATE table SET col_a = ?`,
 			expectArgsCount:     1,
 			expectArgNamesCount: 1,
-			inArgs: map[string]any{
-				"a": "a value",
+			inArgs: []any{
+				map[string]any{
+					"a": "a value",
+				},
 			},
 			expectOutArgs: []any{"a value"},
 		},
@@ -142,8 +267,10 @@ func TestNamedTemplate(t *testing.T) {
 			option:              PostgresOption,
 			expectArgsCount:     1,
 			expectArgNamesCount: 1,
-			inArgs: map[string]any{
-				"a": "a value",
+			inArgs: []any{
+				map[string]any{
+					"a": "a value",
+				},
 			},
 			expectOutArgs: []any{"a value"},
 		},
@@ -188,17 +315,17 @@ func TestNamedTemplate(t *testing.T) {
 						if tc.omissibleArgs != nil {
 							nt.OmissibleArgs(tc.omissibleArgs...)
 						}
-						outArgs, err := nt.Args(tc.inArgs)
+						outArgs, err := nt.Args(tc.inArgs...)
 						if tc.expectArgsError {
 							assert.Error(t, err)
 							assert.Panics(t, func() {
-								_ = nt.MustArgs(tc.inArgs)
+								_ = nt.MustArgs(tc.inArgs...)
 							})
 						} else {
 							assert.NoError(t, err)
 							assert.Equal(t, tc.expectOutArgs, outArgs)
 							assert.NotPanics(t, func() {
-								_ = nt.MustArgs(tc.inArgs)
+								_ = nt.MustArgs(tc.inArgs...)
 							})
 						}
 					})
@@ -206,6 +333,12 @@ func TestNamedTemplate(t *testing.T) {
 			}
 		})
 	}
+}
+
+type unmarshalable struct{}
+
+func (u *unmarshalable) MarshalJSON() ([]byte, error) {
+	return nil, errors.New("fooey")
 }
 
 func TestNamedTemplate_DefaultValue(t *testing.T) {
@@ -232,4 +365,119 @@ func TestNamedTemplate_DefaultValue(t *testing.T) {
 	assert.Equal(t, 2, len(args))
 	assert.Equal(t, "a value", args[0])
 	assert.True(t, (args[1].(time.Time)).After(now))
+}
+
+func TestNamedTemplate_Clone(t *testing.T) {
+	nt := MustCreateNamedTemplate(`INSERT INTO table (col_a, col_b, col_c) VALUES (:a, :b, :a)`, nil).
+		DefaultValue("a", "a default").
+		OmissibleArgs("b")
+	assert.Equal(t, `INSERT INTO table (col_a, col_b, col_c) VALUES (?, ?, ?)`, nt.Statement())
+	assert.Equal(t, []any{"a default", nil, "a default"}, nt.MustArgs(map[string]any{}))
+
+	nt2 := nt.Clone(nil)
+	assert.Equal(t, `INSERT INTO table (col_a, col_b, col_c) VALUES (?, ?, ?)`, nt2.Statement())
+	assert.Equal(t, []any{"a default", nil, "a default"}, nt2.MustArgs(map[string]any{}))
+
+	nt2 = nt.Clone(PostgresOption)
+	assert.Equal(t, `INSERT INTO table (col_a, col_b, col_c) VALUES ($1, $2, $1)`, nt2.Statement())
+	assert.Equal(t, []any{"a default", nil}, nt2.MustArgs(map[string]any{}))
+}
+
+func TestFoo(t *testing.T) {
+	db, mock, _ := sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual))
+
+	mock.ExpectQuery("SELECT name FROM users WHERE id in (?, ?) limit 1").WithArgs(1, 2).WillReturnError(fmt.Errorf("some error"))
+	row := db.QueryRow("SELECT name FROM users WHERE id in (?, ?) limit 1", 1, 2)
+	println(row.Err().Error())
+	if err := mock.ExpectationsWereMet(); err != nil {
+		fmt.Printf("there were unfulfilled expectations: %s", err)
+	}
+}
+
+func TestNamedTemplate_Exec(t *testing.T) {
+	nt, err := NewNamedTemplate(`INSERT INTO table (col_a, col_b, col_c) VALUES (:a, :b, :a)`, nil)
+	require.NoError(t, err)
+	db, mock, err := sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual))
+	require.NoError(t, err)
+	mock.ExpectExec(nt.Statement()).
+		WithArgs("aa", "bb", "aa").
+		WillReturnResult(sqlmock.NewResult(1, 1))
+
+	_, err = nt.Exec(db, map[string]any{
+		"a": "aa",
+		"b": "bb",
+	})
+	assert.NoError(t, err)
+	assert.NoError(t, mock.ExpectationsWereMet())
+
+	_, err = nt.Exec(db, map[string]any{
+		"a": "aa",
+	})
+	assert.Error(t, err)
+}
+
+func TestNamedTemplate_ExecContext(t *testing.T) {
+	nt, err := NewNamedTemplate(`INSERT INTO table (col_a, col_b, col_c) VALUES (:a, :b, :a)`, nil)
+	require.NoError(t, err)
+	db, mock, err := sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual))
+	require.NoError(t, err)
+	mock.ExpectExec(nt.Statement()).
+		WithArgs("aa", "bb", "aa").
+		WillReturnResult(sqlmock.NewResult(1, 1))
+
+	_, err = nt.ExecContext(context.Background(), db, map[string]any{
+		"a": "aa",
+		"b": "bb",
+	})
+	assert.NoError(t, err)
+	assert.NoError(t, mock.ExpectationsWereMet())
+
+	_, err = nt.ExecContext(context.Background(), db, map[string]any{
+		"a": "aa",
+	})
+	assert.Error(t, err)
+}
+
+func TestNamedTemplate_Query(t *testing.T) {
+	nt, err := NewNamedTemplate(`SELECT * FROM table WHERE col_a = :a OR col_a = :b AND col_c = :a`, nil)
+	require.NoError(t, err)
+	db, mock, err := sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual))
+	require.NoError(t, err)
+	mock.ExpectQuery(nt.Statement()).
+		WithArgs("aa", "bb", "aa").
+		WillReturnRows()
+
+	_, err = nt.Query(db, map[string]any{
+		"a": "aa",
+		"b": "bb",
+	})
+	assert.NoError(t, err)
+	assert.NoError(t, mock.ExpectationsWereMet())
+
+	_, err = nt.Query(db, map[string]any{
+		"a": "aa",
+	})
+	assert.Error(t, err)
+}
+
+func TestNamedTemplate_QueryContext(t *testing.T) {
+	nt, err := NewNamedTemplate(`SELECT * FROM table WHERE col_a = :a OR col_a = :b AND col_c = :a`, nil)
+	require.NoError(t, err)
+	db, mock, err := sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual))
+	require.NoError(t, err)
+	mock.ExpectQuery(nt.Statement()).
+		WithArgs("aa", "bb", "aa").
+		WillReturnRows()
+
+	_, err = nt.QueryContext(context.Background(), db, map[string]any{
+		"a": "aa",
+		"b": "bb",
+	})
+	assert.NoError(t, err)
+	assert.NoError(t, mock.ExpectationsWereMet())
+
+	_, err = nt.QueryContext(context.Background(), db, map[string]any{
+		"a": "aa",
+	})
+	assert.Error(t, err)
 }
